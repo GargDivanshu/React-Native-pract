1ddbe420e345d0cac835e4a1ca7f6fe2a0051743
GargDivanshu <divanshu.garg113@gmail.com>
2023-04-10 02:06:28 +0530

    finalisation

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,30 @@
-import {stack } from 'expo-router'
+import {Stack } from 'expo-router'
+import {useCallback} from 'react'
+import {useFonts} from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync();
+
 
 const Layout = () => {
-    
+   const [fontsLoaded] = useFonts({
+      DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+      DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+      DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+   })
+
+   const onLayoutRootView = useCallback(async () => {
+      if (fontsLoaded) {
+         await SplashScreen.hideAsync();
+      }
+
+      if(!fontsLoaded) {
+         return null
+      }
+   }, [fontsLoaded])
+   return <Stack
+      onLayoutRootView={onLayoutRootView}
+   />
 }
+
+export default Layout
